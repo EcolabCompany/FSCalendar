56ac021ed13aceb5d256cd4346cb1bf971f95905
Nate <nathan.mann@me.com>
2019-12-17 14:37:49 -0500

    removed .dynamic

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "FSCalendar",
-            type: .dynamic,
+//            type: .dynamic,
             targets: ["FSCalendar"])
     ],
     targets: [
